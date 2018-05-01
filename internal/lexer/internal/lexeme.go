d1dc84208de381144f99c9597104283c43ed5476
Yoseph Radding <yoseph.radding@bankrate.com>
2018-05-01 10:10:35 -0400

    removed unnecessary code from the lexeme file

--- internal/lexer/internal/lexeme.go
+++ internal/lexer/internal/lexeme.go
@@ -1,8 +1,6 @@
 package internal
 
 import (
-	"regexp"
-
 	"github.com/radding/ArborGo/internal/tokens"
 )
 
@@ -10,26 +8,4 @@ import (
 type Lexeme struct {
 	Token tokens.Token
 	Value string
-}
-
-//Tokenizer is what takes the string and turns it into a lexeme
-type Tokenizer func(value string) (*Lexeme, error)
-
-//RegisterLexeme returns a function that can identify matches and returns the lexeme
-func RegisterLexeme(regexVal string, token tokens.Token) Tokenizer {
-	regex, err := regexp.Compile(regexVal)
-
-	return func(value string) (*Lexeme, error) {
-		if err != nil {
-			return nil, err
-		}
-		matched := regex.Match([]byte(value))
-		if matched {
-			return &Lexeme{
-				Token: token,
-				Value: value,
-			}, nil
-		}
-		return nil, nil
-	}
 }
