05edb770ff7c7da2b5dd52466dbc095d954b55e0
Yoseph Radding <yoseph@shuttl.io>
2019-01-27 19:06:30 -0500

    Temp

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/arborlang/ArborGo
 
 replace github.com/go-interpreter/wagon v0.0.0 => github.com/perlin-network/wagon v0.3.1-0.20180825141017-f8cb99b55a39
+replace github.com/perlin-network/life v0.0.0-20181215083421-28a99a6d79ec => ../../radding/life
 
 require (
 	github.com/arborlang/arbor-dev v0.0.0-20190126210124-ccaaedf77120
--- internal/parser/visitors/WatCompiler/VisitArray.go
+++ internal/parser/visitors/WatCompiler/VisitArray.go
@@ -24,6 +24,10 @@ func (c *Compiler) VisitSliceNode(node *ast.SliceNode) (ast.VisitorMetaData, err
 	c.EmitFunc(";; Slicing an array")
 	// c.stackPointer += 4
 	// location := c.stackPointer
+	arrayPosition := c.getUniqueID("current_array", "position")
+	arraySize := c.getUniqueID("current_array", "size")
+	c.AddLocal(arrayPosition, "i32")
+	c.AddLocal(arraySize, "i32")
 	localName := c.getUniqueID("stack", "pointer")
 	c.AddLocal(localName, "i32")
 	c.EmitFunc("i64.const 4")
@@ -35,13 +39,54 @@ func (c *Compiler) VisitSliceNode(node *ast.SliceNode) (ast.VisitorMetaData, err
 	if err != nil {
 		return ast.VisitorMetaData{}, err
 	}
+	c.EmitFunc("tee_local %s", arrayPosition)
 	c.EmitFunc("i32.load")
+	c.EmitFunc("tee_local %s", arraySize)
 	c.EmitFunc("i32.const 1")
 	c.EmitFunc("i32.sub")
 	c.EmitFunc("i32.store")
+
+	loop := c.getLabelUniqueID("loop")
+	loopEnd := c.getLabelUniqueID("loopEnd")
+	// Start the block for the loop
+	c.EmitFunc("block %s", loop)
+	c.EmitFunc("loop %s", loopEnd)
+
+	// Get the array size. If it is equal to zero, break out of the loop
+	c.EmitFunc("get_local %s", arraySize)
+	c.EmitFunc("i32.eqz")
+	c.EmitFunc("br_if %s", loop)
+
+	// First add four to the arrayPostition
+	c.EmitFunc("i32.const 4")
+	c.EmitFunc("get_local %s", arrayPosition)
+	c.EmitFunc("i32.add")
+
+	// Store the new pointer, but also we need to load it
+	c.EmitFunc("tee_local %s", arrayPosition)
+	c.EmitFunc("i32.load")
+
+	//Increment the stack by 4
+	c.EmitFunc("i64.const 4")
+	c.EmitFunc("call $__allocstack__")
+	// Store the value at the new position
+	// c.EmitFunc("")
+	c.EmitFunc("i32.store")
+	//Decrement the arraysize by 1
+	c.EmitFunc("get_local %s", arraySize)
+	c.EmitFunc("i32.const 1")
+	c.EmitFunc("i32.sub")
+	c.EmitFunc("set_local %s", arraySize)
+	// Jump back to the start of the loop
+	c.EmitFunc("br %s", loopEnd)
+	c.EmitFunc("end %s", loopEnd)
+	c.EmitFunc("end %s", loop)
+
 	// c.stackPointer += 4
 	// c.EmitFunc("i32.const %d", location)
 	// c.EmitFunc("call $__")
+
+	//Must be last command to emit
 	c.EmitFunc("get_local %s", localName)
 	c.EmitFunc(";; Done slicing an array")
 	return ast.VisitorMetaData{}, nil
